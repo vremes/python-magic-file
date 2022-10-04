5b09bd6b17aa64ee81d199087530964751609d2c
vremes <valtteriremes@outlook.com>
2022-10-05 00:32:30 +0300

    Fix imports.

--- python_magic_file/__init__.py
+++ python_magic_file/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ['MagicFile', 'MagicFileException', 'UnreadableFileException', 'NotBinaryFileException', 'NotSeekableFileException']
 
 from .file import MagicFile
-from .exceptions import MagicFileException, UnreadableFileException, NotBinaryFileException, NotSeekableFileException
+from .exceptions import MagicFileException, NotBinaryFileException
