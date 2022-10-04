37778cd74b0bebfa5c93cecdce617b9f2d00c2ca
vremes <valtteriremes@outlook.com>
2022-10-05 00:04:30 +0300

    Rename folder.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="magic_file",
+    name="python_magic_file",
     version="0.0.1",
     author="Valtteri Remes",
     description="Small Python module to aid developers in getting file extensions from files securely.",
@@ -16,7 +16,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
     ],
     python_requires='>=3.6',
-    py_modules=["magic_file"],
+    py_modules=["python_magic_file"],
     install_requires=[
         "python-magic == 0.4.27; platform_system != 'Windows'",
         "python-magic-bin == 0.4.14; platform_system == 'Windows'"
